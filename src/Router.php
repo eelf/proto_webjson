9dda000072f3150e81a68e652dfe175a834848b4
Evgeniy Makhrov <e.makhrov@corp.badoo.com>
2020-04-23 15:54:39 +0300

    binary wire format

--- src/Router.php
+++ src/Router.php
@@ -3,67 +3,17 @@
 namespace Eelf\WebJson;
 
 class Router {
-    private function jsonToMessage($json, \Eelf\Protobuf\Message $message) {
-        foreach ($message::desc() as $tag => ['name' => $name, 'type' => $type, 'repeated' => $repeated]) {
-            if (!array_key_exists($name, $json)) continue;
-
-            if ($repeated) {
-                if (!is_array($json[$name])) throw new \Exception('repeated expected got ' . gettype($json[$name]));
-                foreach ($json[$name] as $item) {
-                    if (is_string($type)) {
-                        $value = new $type;
-                        $this->jsonToMessage($item, $value);
-                    } else {
-                        $value = $item;
-                    }
-                    $message->fields[$name][] = $value;
-                }
-                continue;
-            }
-
-            if (is_string($type)) {
-                $value = new $type;
-                $this->jsonToMessage($json[$name], $value);
-            } else {
-                $value = $json[$name];
-            }
-            $message->fields[$name] = $value;
+    public function __invoke($method, $message, $service) {
+        $class = (new \ReflectionMethod($service, $method))->getParameters()[0]->getClass()->getName();
+        [$request, $err] = $class::fromBytes($message);
+        if ($err !== null) {
+            throw $err;
         }
-    }
-    private function messageToJson(\Eelf\Protobuf\Message $message) : array {
-        $res = [];
-        foreach ($message::desc() as $tag => ['name' => $name, 'type' => $type, 'repeated' => $repeated]) {
-            if (!array_key_exists($name, $message->fields)) continue;
-            if (is_string($type)) {
-                if ($repeated) {
-                    $value = array_map([$this, 'messageToJson'], $message->fields[$name]);
-                } else {
-                    $value = $this->messageToJson($message->fields[$name]);
-                }
-            } else {
-                $value = $message->fields[$name];
-            }
-            $res[$tag - 1] = $value;
+
+        [$response, $err] = $service->$method($request)->toBytes();
+        if ($err !== null) {
+            throw $err;
         }
-        return $res;
-    }
-    public function __invoke($body, $service) {
-        [$method, $message] = explode(' ', $body, 2);
-        $m = json_decode($message, true);
-        $Rm = new \ReflectionMethod($service, $method);
-        $Rp = $Rm->getParameters()[0];
-        $class = $Rp->getClass()->getName();
-        $request = new $class;
-
-        try {
-            $this->jsonToMessage($m, $request);
-
-            $response = $service->$method($request);
-
-            $json = $this->messageToJson($response);
-        } catch (\Throwable $e) {
-            return "FAIL $e";
-        }
-        return 'OK ' . json_encode($json);
+        return $response;
     }
 }
